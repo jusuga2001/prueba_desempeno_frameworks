3e753fd7232c8cf78e6c9f5edbd5e3b2c2e5f0e5
Julian Acero <julianusuga95@gmail.com>
2024-06-05 06:26:15 -0500

    primer commit

---
+++ src/test/java/com/riwi/prueba_desempeno/PruebaDesempenoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riwi.prueba_desempeno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaDesempenoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
